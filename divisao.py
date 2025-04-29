7418a43a124a93df4e5cfabd00b2c53b116cee5a
NicolasF <nicolasfabriciosojogos@gmail.com>
2025-04-29 20:59:26 -0300

    criaçao do arquivo divisao.py

---
+++ divisao.py
@@ -0,0 +1,6 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+if b != 0:
+    print("Resultado da divisão:", a / b)
+else:
+    print("Não é possível dividir por zero.")
